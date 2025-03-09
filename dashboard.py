4a4c57586e26f3770829f18862a05d555af478e5
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2025-03-09 15:39:36 +0700

    perbaikan error

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ def main():
     st.title("Selamat datang di Dashbard persewaan sepeda")
     st.subheader("Dashboard ini menampilkan analisis data persewaan sepedan untuk tugas Dicoding")
     st.write("Pilihlah menu yang berada pada sebelah kiri untuk menggunakan dashboard ini!")
-    st.image("./images/5243.jpg", caption="Image by pch.vector on Freepik", use_column_width=True)
+    st.image("./images/5243.jpg", caption="Image by pch.vector on Freepik", use_container_width=True)
 
 #########################
 # menjalankan kode main #
